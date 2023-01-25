4faca554a3214e51deca9df89ffbada2f7f0b427
Camryn <camrynpatterson@yahoo.com>
2023-01-25 14:41:48 -0600

    adding an array for songs

--- index.js
+++ index.js
@@ -7,3 +7,5 @@ const playing = document.querySelector('#playing')
 const progressContainer = document.querySelector('.progress-container')
 const music = document.querySelector('#music')
 const progress = document.querySelector('.progress')
+
+const songs = ['you missed my heart', 'georgia', 'funeral']
